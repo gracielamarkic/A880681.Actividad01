437b005eb4b3a3862961b7bef25d06612cf5447e
gracielamarkic <gracielamarkic@hotmail.com>
2021-03-29 17:59:40 -0300

    .

---
+++ A880681.Actividad01/Ejercicios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A880681.Actividad01
+{
+    class Ejercicios
+    {
+        public void Saludar()
+        {
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
---
+++ A880681.Actividad01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace A880681.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejercicios E = new Ejercicios();
+
+            E.Saludar();
+
+            Console.WriteLine("Presione enter para salir");
+            Console.ReadLine();
+        }
+    }
+}
